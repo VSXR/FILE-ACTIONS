1ae4a6f6d859c05df6a044504091d48cf0743469
Victor Sanz <131462560+VSNZ@users.noreply.github.com>
2023-11-12 12:36:16 +0100

    PROGRAM IDEA

---
+++ program.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+
+int Menu();
+
+
+int main(void){
+    int op;
+    char response[4];
+    FILE *pFile;
+
+    
+        
+    
+    printf("-- PROGRAM FINISHED --");
+    return 0;
+}
+
+
+int Menu(){
+    
+}
